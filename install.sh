8e31b461341bed1dbfade212ffa0d7b06ca70dd7
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2024-10-24 21:34:04 -0300

    type: Add File file: install.sh description: add instalation file.

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+
+echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.bashrc
+echo 'alias v=nvim' >> ~/.bashrc
+source ~/.bashrc
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+/opt/nvim-linux64/bin/nvim -c PlugInstall
