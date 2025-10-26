7c85ee8d333e24d81d4f82c728d768109cb16c79
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2025-10-25 22:20:07 -0300

    refactory: uperkeep install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
-sudo apt install nodejs npm -y
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
 
-echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.bashrc
+NEOVIM_HOME=~/.neovim
+NEOVIM_CONFIG=~/.config/nvim
+
+rm -rf $NEOVIM_CONFIG
+rm -rf $NEOVIM_HOME
+mkdir -p $NEOVIM_CONFIG
+mkdir $NEOVIM_HOME
+cp -r ./init.vim ./install.sh ./coc-settings.json $NEOVIM_CONFIG 
+
+#install node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+nvm install
+
+#nvim install
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo tar -C $NEOVIM_HOME -xzf nvim-linux-x86_64.tar.gz
+
+echo 'export PATH="$PATH:~/.neovim/nvim-linux64/bin"' >> ~/.bashrc
 echo 'alias v=nvim' >> ~/.bashrc
 source ~/.bashrc
 
+# install VimPlug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-/opt/nvim-linux64/bin/nvim -c PlugInstall
+$NEOVIM_HOME/nvim-linux64/bin/nvim -c PlugInstall
